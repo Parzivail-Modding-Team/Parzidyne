7807c87a66a5b0ab3f78e11fe826fd1e496b3470
parzivail <colby.newman2000@gmail.com>
2018-10-23 19:02:48 -0400

    added new textures

--- src/main/java/com/parzivail/parzidyne/registry/CraftingRegistry.java
+++ src/main/java/com/parzivail/parzidyne/registry/CraftingRegistry.java
@@ -2,6 +2,8 @@ package com.parzivail.parzidyne.registry;
 
 import com.parzivail.parzidyne.registry.recipe.CentrifugeRecipes;
 import com.parzivail.parzidyne.registry.recipe.CompressorRecipes;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class CraftingRegistry
@@ -9,6 +11,7 @@ public class CraftingRegistry
 	public static void register()
 	{
 		addCompressor(new ItemStack(ItemRegister.carbonFiberComposite, 8), new ItemStack(ItemRegister.carbonFiber));
+		addCompressor(new ItemStack(Blocks.coal_block, 16), new ItemStack(Items.diamond));
 	}
 
 	private static void addCompressor(ItemStack in, ItemStack out)
